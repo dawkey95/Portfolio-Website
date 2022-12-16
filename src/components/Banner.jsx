ee780b7806cd1401fe1f2c867dad3a3875e40388
dawkey95 <dawidkeyser95@gmail.com>
2022-12-17 10:03:41 +1300

    Text update with family and spacing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,59 @@
+import { useState, useEffect } from 'react';
 import { Col, Row, Container } from 'react-bootstrap';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import HeaderImage from '../assets/img/panda-astronaut.png';
 
 const Banner = () => {
+	const [loopNum, setLoopNum] = useState(0);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [text, setText] = useState('');
+	const [delta, setDelta] = useState(300 );
+	const toRotate = ['Web Developer', 'Web Designer', 'Gamer'];
+	const period = 2000;
+
+	useEffect(() => {
+		let ticker = setInterval(() => {
+			tick();
+		}, delta);
+
+		return () => {
+			clearInterval(ticker);
+		};
+	}, [text]);
+
+	const tick = () => {
+		let i = loopNum % toRotate.length;
+		let fullText = toRotate[i];
+		let updatedText = isDeleting
+			? fullText.substring(0, text.length - 1)
+			: fullText.substring(0, text.length + 1);
+
+		setText(updatedText);
+
+		if (isDeleting) {
+			setDelta((prevDelta) => prevDelta / 2);
+		}
+
+		if (!isDeleting && updatedText === fullText) {
+			setIsDeleting(true);
+			setDelta(period);
+		} else if (isDeleting && updatedText === '') {
+			setIsDeleting(false);
+			setLoopNum(loopNum + 1);
+			setDelta(500);
+		}
+	};
+
 	return (
 		<section className="banner" id="home">
 			<Container>
 				<Row className="align-items-center">
 					<Col xs={12} md={6} xl={7}>
-						<span className="tagline"> Welcome to my Portfolio</span>
+						<span className="tagline">Welcome to my Portfolio</span>
 						<h1>
 							{`Hi I'm Dawid `}
-							<span className="wrap">web developer</span>
+              <br></br>
+							<span className="wrap span-text">{text}</span>
 						</h1>
 						<p>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit id atque
@@ -29,8 +71,8 @@ const Banner = () => {
 						</button>
 					</Col>
 					<Col xs={12} md={6} xl={5}>
-            <img src={HeaderImage} alt="Header Image" />
-          </Col>
+						<img src={HeaderImage} alt="Header" />
+					</Col>
 				</Row>
 			</Container>
 		</section>
